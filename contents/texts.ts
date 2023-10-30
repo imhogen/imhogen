3b3719e7b0aca7fc5d431ce20759bb1a61883155
kennyAnyi9 <kennyanyi9@gmail.com>
2023-10-30 13:49:01 +0000

    🚚: move exports and contect texts into  single files

---
+++ contents/texts.ts
@@ -0,0 +1,5 @@
+const texts = {
+  home: " At our core, we are masters of mechanical engineering design. Our prowess empowers us to craft designs that yield robust,high-performing products and streamlined processes.",
+};
+
+export default texts;
---
+++ src/app/components/exports.ts
@@ -0,0 +1,13 @@
+import Navbar from "./layout/desktop-navigation";
+import Dropdown from "./layout/mobile-navigation";
+import Slider from "./layout/tablet-navigation";
+import Landing from "./sections/landing";
+
+const exports = {
+  Dropdown,
+  Landing,
+  Slider,
+  Navbar,
+};
+
+export { Dropdown, Landing, Slider, Navbar };

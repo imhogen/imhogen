7d03c79810ffe6978395d3bc5f15aa79687edd09
kennyAnyi9 <kennyanyi9@gmail.com>
2023-11-03 10:06:05 +0000

    chore: rename or 🚚move files to reorganize folder structure

--- contents/texts.ts
+++
@@ -1,5 +0,0 @@
-const texts = {
-  home: " At our core, we are masters of mechanical engineering design. Our prowess empowers us to craft designs that yield robust,high-performing products and streamlined processes.",
-};
-
-export default texts;

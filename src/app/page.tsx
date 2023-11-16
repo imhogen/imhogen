1eea82d52a4e4a415af6301830045c8d72d9a9ed
abdul-manan <mahamamanan8@gmail.com>
2023-11-16 07:26:37 +0000

    got rid of horizontal scroll in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main
       className="flex min-h-screen flex-col justify-center items-center overflow-x-hidden overflow-y-hidden
-                  max-w-screen mx-auto "
+                  max-w-full mx-auto "
     >
       {/* Home */}
       <Shell className="relative lg:top-0 gap-10 lg:gap-36  ">

ae63c57b9e5703a95f40600ab313b376aedc0b5a
kennyAnyi9 <kennyanyi9@gmail.com>
2023-12-08 16:21:58 +0000

    configure vercel to ignore actions folder and remove experimental flag from next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-
-    esmExternals: false,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;

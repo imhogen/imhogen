a55fa8d9b2d9372311111eab8f6b464b99550b93
kennyAnyi9 <kennyanyi9@gmail.com>
2023-12-19 03:20:33 +0000

    apply motion effect to blog landing page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,7 @@ import { client } from "../components/lib/sanity";
 import { BlogPost } from "../../../Types";
 import Link from "next/link";
 import Image from "next/image";
+import Motion, { childVariants } from "../framer";
 
 async function getProjects() {
   const query = `*[_type == "blog"]{
@@ -29,43 +30,45 @@ export default async function page() {
   );
   return (
     <div className="w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-fit gap-10 ">
-        {data.map((posts) => (
-          <article
-            key={posts._id}
-            className="w-full flex-1 p-2 h-fit   bg-white rounded-lg flex-col justify-center items-start gap-4 inline-flex font-redhat"
-          >
-            <Image
-              className=" h-fit rounded-lg w-full"
-              src={posts.imageUrl}
-              height={100}
-              width={300}
-              alt="blog-card-picture"
-            />
-            <div className="  flex-col justify-center items-start gap-2 flex">
-              <Link href={`/posts/${posts.slug.current}`} prefetch>
-                <div className=" text-black text-lg font-extrabold leading-snug font-machina line-clamp-2">
-                  {posts.title}
-                </div>
-              </Link>
+      <Motion childVariants={childVariants}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-fit gap-10 ">
+          {data.map((posts) => (
+            <article
+              key={posts._id}
+              className="w-full flex-1 p-2 h-fit   bg-white rounded-lg flex-col justify-center items-start gap-4 inline-flex font-redhat"
+            >
+              <Image
+                className=" h-fit rounded-lg w-full"
+                src={posts.imageUrl}
+                height={100}
+                width={300}
+                alt="blog-card-picture"
+              />
+              <div className="  flex-col justify-center items-start gap-2 flex">
+                <Link href={`/posts/${posts.slug.current}`} prefetch>
+                  <div className=" text-black text-lg font-extrabold leading-snug font-machina line-clamp-2">
+                    {posts.title}
+                  </div>
+                </Link>
 
-              <div className=" text-black text-opacity-70 text-base font-normal  leading-normal line-clamp-3">
-                {posts.overview}
+                <div className=" text-black text-opacity-70 text-base font-normal  leading-normal line-clamp-3">
+                  {posts.overview}
+                </div>
               </div>
-            </div>
-            <div className="py-2 rounded-md justify-center items-center gap-2.5 inline-flex">
-              <Link
-                href={`/posts/`}
-                prefetch
-                className="text-amber-600 text-base font-semibold  leading-tight font-redhat"
-              >
-                READ ME{" >"}
-              </Link>
-              <div className="w-4 h-4 relative" />
-            </div>
-          </article>
-        ))}
-      </div>
+              <div className="py-2 rounded-md justify-center items-center gap-2.5 inline-flex">
+                <Link
+                  href={`/posts/`}
+                  prefetch
+                  className="text-amber-600 text-base font-semibold  leading-tight font-redhat"
+                >
+                  READ ME{" >"}
+                </Link>
+                <div className="w-4 h-4 relative" />
+              </div>
+            </article>
+          ))}
+        </div>
+      </Motion>
     </div>
   );
 }

f7644ea9676e53bd0f5dcf28dff82182140a0ce3
kennyAnyi9 <kennyanyi9@gmail.com>
2024-01-02 10:19:06 +0000

    style: refactor styles for blog and blog detail pages

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,13 +1,10 @@
 import { client } from "@/app/components/lib/sanity";
-import Navbar from "@/app/components/navigation/desktop-navigation";
-import Dropdown from "@/app/components/navigation/mobile-navigation";
-import Slider from "@/app/components/navigation/tablet-navigation";
 import React from "react";
 import { BlogPost } from "../../../../Types";
-import { Shell } from "@/app/components/ui/shell";
 import PortableText from "react-portable-text";
 import Image from "next/image";
 import { machina, redhat } from "@/app/components/exports";
+import notFound from "@/app/not-found";
 
 async function getPostDetails(slug: string) {
   // const query = `*[_type == "blog" && slug.current == "${slug}"][0]`;
@@ -25,17 +22,11 @@ async function getPostDetails(slug: string) {
   }[0]`;
 
   const data = await client.fetch(query);
-  console.log(data);
+
   return data;
 }
 export const revalidate = 60;
 
-function estimateReadingTime(text: string) {
-  const wordsPerMinute = 200; // average reading speed
-  const numberOfWords = text.toString().split(/\s/g).length; // splits the text by spaces and counts the words
-  const minutes = numberOfWords / wordsPerMinute;
-  return Math.ceil(minutes); // rounds up to the nearest minute
-}
 export default async function Posts({
   params,
 }: {
@@ -44,61 +35,54 @@ export default async function Posts({
   };
 }) {
   const data = (await getPostDetails(params.slug)) as BlogPost;
-  console.log("working out actually and i am very excited");
+
+  const wordsPerMinute = 200;
+  const numberOfWords = data.content.toString().split(/\s/g).length;
+  const minutes = Math.ceil(numberOfWords / wordsPerMinute);
+  console.log(minutes, numberOfWords);
+
+  // if (!data) {
+  //   notFound();
+  // }
   return (
-    <main
-      className="flex h-full flex-col justify-center items-center overflow-x-hidden overflow-y-hidden
-                max-w-full mx-auto "
-    >
-      <Shell
-        className={` relative top-12 h-full lg:min-h-screen md:top-28 gap-10 ${redhat.className}`}
+    <main className="flex flex-col  gap-3 mb-20 w-11/12 overflow-x-hidden  lg:w-3/5 items-center mx-auto relative top-24 prose prose-md lg:prose-xl prose-invert prose-a:text-gray-300 md:top-36">
+      <h1
+        className={`text-center text-xl md:text-2xl lg:text-4xl tracking-tight leading-relaxed text-gray-100 ${machina.className}`}
       >
-        <div className="flex flex-col gap-5 w-full h-full lg:min-h-screen lg:w-4/5  mx-auto">
-          <h1
-            className={` text-center text-xl md:text-3xl lg:text-5xl tracking-tight leading-relaxed text-gray-100 ${machina.className}`}
-          >
-            {data.title}
-          </h1>
+        {data.title}
+      </h1>
 
-          <div className="flex justify-center gap-5">
-            <div>
-              <Image
-                src="/images/golden-eye.webp"
-                alt="author-image"
-                height={50}
-                width={50}
-                className="rounded-full"
-              />
-            </div>
-            <div className="flex flex-col justify-center  ">
-              <p className="text-sm text-gray-400 text-left opacity-50 ">
-                {data.author}
-              </p>
-              <p className="text-sm text-gray-400 text-left opacity-50 ">
-                {`${
-                  new Date(data._createdAt).toISOString().split("T")[0]
-                } . ${estimateReadingTime(data.content)} min read`}
-              </p>
-            </div>
-          </div>
+      <div className="flex justify-center gap-5">
+        <Image
+          src="/images/golden-eye.webp"
+          alt="author-image"
+          height={50}
+          width={50}
+          className="rounded-full"
+        />
 
-          <article className="flex gap-10 min-h-screen flex-col text-gray-500 text-md">
-            <Image
-              className=" mx-auto w-full h-56 md:h-64 lg:h-96 rounded-lg "
-              src={data.imageUrl}
-              height={100}
-              width={300}
-              alt="blog-card-picture"
-            />
-            <div className="relative">
-              <PortableText
-                content={data.content}
-                // serializers={PortableTextComponent}
-              />
-            </div>
-          </article>
+        <div className="flex flex-col justify-center">
+          <p className="text-sm text-gray-400 text-left opacity-50 ">
+            {data.author}
+            <br />
+            {`${new Date(data._createdAt).toISOString().split("T")[0]}`}
+          </p>
         </div>
-      </Shell>
+      </div>
+      <Image
+        className=" rounded-lg "
+        src={data.imageUrl}
+        height={800}
+        width={800}
+        alt="blog-card-picture"
+      />
+
+      <article className={`w-full text-gray-500 text-md ${redhat.className}`}>
+        <PortableText
+          content={data.content}
+          // serializers={PortableTextComponent}
+        />
+      </article>
     </main>
   );
 }
